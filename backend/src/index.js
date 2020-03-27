e781a307c3320c0ca036fdfd4ce0390e7c2efe86
William Koller <wkoller25@gmail.com>
2020-03-27 01:30:46 -0300

    feat: add cors

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require('morgan');
+const cors = require('cors');
+const route = require('./routes');
+const app = express();
+app.use(cors())
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(route);
+
+app.listen(port, (req, res) => {
+  console.log(`server running ${port}`);
+});
