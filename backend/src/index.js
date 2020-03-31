635cf5d3233228ba056a6991603743157cc2b776
William Koller <wkoller25@gmail.com>
2020-03-30 23:13:01 -0300

    feat: alter celebrate on routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const route = require('./routes');
 const app = express();
 app.use(cors())
@@ -9,6 +10,7 @@ const port = 3333;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(route);
+app.use(errors());
 
 app.listen(port, (req, res) => {
   console.log(`server running ${port}`);
